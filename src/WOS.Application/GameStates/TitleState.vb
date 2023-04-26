fe05c93425f1cb226e3685176278dbb2a19228ca
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-04-25 22:44:34 -0500

    a flashing title screen, and i should go to bed

--- src/WOS.Application/GameStates/TitleState.vb
+++ src/WOS.Application/GameStates/TitleState.vb
@@ -1,16 +1,17 @@
-﻿Friend Class TitleState
-    Inherits BaseGameState(Of Hue, Command, Sfx, GameState)
+﻿Imports System.Security.Cryptography
 
+Friend Class TitleState
+    Inherits BaseGameState(Of Hue, Command, Sfx, GameState)
     Public Sub New(parent As IGameController(Of Hue, Command, Sfx), setState As Action(Of GameState?, Boolean))
         MyBase.New(parent, setState)
     End Sub
-
     Public Overrides Sub HandleCommand(command As Command)
     End Sub
-
+    Const TitleText = "Wonting of SPLORR!!"
     Public Overrides Sub Render(displayBuffer As IPixelSink(Of Hue))
         displayBuffer.Fill((0, 0), (ViewWidth, ViewHeight), Hue.Black)
         Dim font = Fonts(GameFont.Font8x8)
-        font.WriteText(displayBuffer, (0, 0), "Title", Hue.White)
+        Dim h = RNG.FromEnumerable(AllHues)
+        font.WriteText(displayBuffer, (ViewWidth \ 2 - font.TextWidth(TitleText) \ 2, ViewHeight \ 2 - font.Height \ 2), TitleText, h)
     End Sub
 End Class
