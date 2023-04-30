dd52fd47fc6473aa1fdd40b7a230751ad79dbf37
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-04-30 15:47:25 -0500

    drop shadow

--- src/WOS.Application/GameStates/InPlay/NavigationState.vb
+++ src/WOS.Application/GameStates/InPlay/NavigationState.vb
@@ -24,6 +24,22 @@
 
     Public Overrides Sub Render(displayBuffer As IPixelSink(Of Hue))
         displayBuffer.Fill((0, 0), (ViewWidth, ViewHeight), Hue.Black)
+        DrawMap(displayBuffer)
+        IndicateMode(displayBuffer)
+        Dim font = Fonts(GameFont.Font3x5)
+        Const text = "FIRE: Change Mode"
+        font.WriteText(displayBuffer, (ViewWidth \ 2 - font.TextWidth(text) \ 2 + 1, ViewHeight - font.Height + 1), text, Hue.Black)
+        font.WriteText(displayBuffer, (ViewWidth \ 2 - font.TextWidth(text) \ 2, ViewHeight - font.Height), text, Hue.DarkGray)
+    End Sub
+
+    Private Shared Sub IndicateMode(displayBuffer As IPixelSink(Of Hue))
+        Dim font = Fonts(GameFont.Font4x6)
+        Const text = "Navigation Mode"
+        font.WriteText(displayBuffer, (ViewWidth \ 2 - font.TextWidth(text) \ 2 + 1, 1), text, Hue.Black)
+        font.WriteText(displayBuffer, (ViewWidth \ 2 - font.TextWidth(text) \ 2, 0), text, Hue.Gray)
+    End Sub
+
+    Private Shared Sub DrawMap(displayBuffer As IPixelSink(Of Hue))
         Dim avatarCharacter = World.Avatar.Character
         Dim offsetX = ViewWidth \ 2 - MapCellWidth \ 2 - avatarCharacter.Column * MapCellWidth
         Dim offsetY = ViewHeight \ 2 - MapCellHeight \ 2 - avatarCharacter.Row * MapCellHeight
