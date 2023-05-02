aeab4e7ac2c33febfc1a59b563274f89e062f098
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-05-02 05:58:09 -0500

    trade item instances

--- src/WOS.Business/Items/Instances/TradeToItemInstance.vb
+++ src/WOS.Business/Items/Instances/TradeToItemInstance.vb
@@ -1,43 +1,18 @@
 ﻿Friend Class TradeToItemInstance
-    Implements IItemInstance
+    Inherits BaseItemInstance
 
-    Private ReadOnly _data As WorldData
     Private ReadOnly _shoppeName As String
     Private ReadOnly _index As Integer
-    Private ReadOnly Property ItemInstanceData As ItemInstanceData
-        Get
-            Return _data.Shoppes(_shoppeName).Trades(_index).ToItem
-        End Get
-    End Property
-
-    Public ReadOnly Property Item As IItem Implements IItemInstance.Item
-        Get
-            Return New Item(_data, ItemInstanceData.ItemName)
-        End Get
-    End Property
-
-    Public Property Quantity As Integer Implements IItemInstance.Quantity
-        Get
-            Return ItemInstanceData.Quantity
-        End Get
-        Set(value As Integer)
-            ItemInstanceData.Quantity = Math.Max(value, 0)
-        End Set
-    End Property
-
-    Public ReadOnly Property CanUse As Boolean Implements IItemInstance.CanUse
-        Get
-            Return Item.CanUse
-        End Get
-    End Property
 
     Public Sub New(data As WorldData, shoppeName As String, index As Integer)
-        _data = data
+        MyBase.New(data)
         _shoppeName = shoppeName
         _index = index
     End Sub
 
-    Public Sub Render(displayBuffer As IPixelSink(Of Hue), x As Integer, y As Integer) Implements IItemInstance.Render
-        Item.Render(displayBuffer, x, y)
-    End Sub
+    Protected Overrides ReadOnly Property ItemInstanceData As ItemInstanceData
+        Get
+            Return _data.Shoppes(_shoppeName).Trades(_index).ToItem
+        End Get
+    End Property
 End Class
