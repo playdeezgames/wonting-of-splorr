d438381a747bc47e07686a7a5a1b5d3385274d9e
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-05-03 05:01:21 -0500

    game items

--- src/WOS.Business/Initializers/GameItems.vb
+++ src/WOS.Business/Initializers/GameItems.vb
@@ -4,12 +4,32 @@
     Friend Const CrownsItemName = "crowns"
     Friend Const JoolsItemName = "jools"
     Friend Const FoodItemName = "chikkin"
+    Friend Const ClubItemName = "club"
+    Friend Const DaggerItemName = "dagger"
+    Friend Const SpearItemName = "spear"
+    Friend Const AxeItemName = "axe"
+    Friend Const WoodenShieldItemName = "woodenshield"
+    Friend Const HelmetItemName = "helmet"
+    Friend Const ShieldItemName = "shield"
+    Friend Const ChainMailItemName = "chainmail"
+    Friend Const PlateMailItemName = "platemail"
+    Friend Const LeatherArmorItemName = "leatherarmor"
     Friend Sub InitializeItems(data As WorldData)
         InitializeHealingItem(data, PotionItemName, " "c, Hue.Red, 5, stacks:=True)
         InitializeItem(data, SwordItemName, "!"c, Hue.DarkGray)
         InitializeItem(data, CrownsItemName, """"c, Hue.Yellow, stacks:=True)
         InitializeItem(data, JoolsItemName, "#"c, Hue.LightCyan, stacks:=True)
-        InitializeHealingItem(data, FoodItemName, "$"c, Hue.Brown, 1, stacks:=True)
+        InitializeItem(data, ClubItemName, "$"c, Hue.Brown, stacks:=True)
+        InitializeItem(data, DaggerItemName, "%"c, Hue.DarkGray, stacks:=True)
+        InitializeItem(data, SpearItemName, "&"c, Hue.Brown, stacks:=True)
+        InitializeItem(data, AxeItemName, "'"c, Hue.DarkGray, stacks:=True)
+        InitializeItem(data, WoodenShieldItemName, "("c, Hue.Brown, stacks:=True)
+        InitializeItem(data, HelmetItemName, ")"c, Hue.DarkGray, stacks:=True)
+        InitializeItem(data, LeatherArmorItemName, "*"c, Hue.Brown, stacks:=True)
+        InitializeItem(data, ShieldItemName, "+"c, Hue.DarkGray, stacks:=True)
+        InitializeItem(data, ChainMailItemName, ","c, Hue.DarkGray, stacks:=True)
+        InitializeItem(data, PlateMailItemName, "-"c, Hue.Gray, stacks:=True)
+        InitializeHealingItem(data, FoodItemName, "."c, Hue.Brown, 1, stacks:=True)
     End Sub
 
     Private Sub InitializeHealingItem(data As WorldData, itemName As String, glyph As Char, hue As Hue, healing As Integer, Optional stacks As Boolean = False)
